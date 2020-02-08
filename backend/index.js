48d1c094daf355e56347bab91304d51ce4d7c8ab
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-02-08 17:41:55 +0100

    implementing helper function on backend

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,37 @@ const router = require('./router');
 const server = http.createServer(app);
 const io = socketio(server);
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
 io.on('connection', socket => {
   console.log('We Have a New Connection');
 
   socket.on('join', ({ name, room }, callback) => {
-    console.log(name, room);
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    //Admin Message
+    socket.emit('message', {
+      user: admin,
+      text: `${user.name},Welcome to the room ${user.room}`
+    });
+
+    socket.broadcast
+      .to(user.room)
+      .emit('message', { user: 'admin', text: `${user.name} has joined` });
+
+    socket.join(user.room);
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
   });
 
   socket.on('disconnect', () => {
