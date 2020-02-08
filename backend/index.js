c1ead9e3bba06a8655697213d8c663986bc7b91f
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-02-08 18:56:19 +0100

    Solving Page Reload Bug

--- backend/index.js
+++ backend/index.js
@@ -44,7 +44,13 @@ io.on('connection', socket => {
   });
 
   socket.on('disconnect', () => {
-    console.log('User had Left');
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit('message', {
+        user: 'admin',
+        text: `${user.name} has left.`
+      });
+    }
   });
 });
 
