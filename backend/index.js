52dd6af5738f2884e72642c387f3dfde289f731b
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-02-08 13:50:49 +0100

    Adding Socket io

--- backend/index.js
+++ backend/index.js
@@ -10,5 +10,13 @@ const router = require('./router');
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', socket => {
+  console.log('We Have a New Connection');
+
+  socket.on('disconnect', () => {
+    console.log('User had Left');
+  });
+});
+
 app.use(router);
 server.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
