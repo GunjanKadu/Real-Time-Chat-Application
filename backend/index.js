ac1f15a9af89762f155738d4df895c9015ce7e11
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-02-08 13:46:39 +0100

    addingRouter

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,10 @@ const http = require('http');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+const router = require('./router');
 
 const server = http.createServer(app);
 const io = socketio(server);
 
+app.use(router);
 server.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
