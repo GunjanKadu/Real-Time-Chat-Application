8652b8d4227bbda5a9879c7e993733537c893ef2
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-02-08 15:41:01 +0100

    gettting url params using query string

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import querystring from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
+  useEffect(() => {
+    const { name, room } = querystring.parse(location.search);
+    console.log(name + ' ' + room);
+    setName(name);
+    setRoom(room);
+  });
   return <h1>Chat</h1>;
 };
 
